248eedffcb3602092e7699d8de44f4c4fbf0ec61
Leonel Gonzalez Vidales <leonelgv@gmail.com>
2019-05-28 00:48:58 -0500

    Feat: Lectura de dos datos de un archivo de texto

--- Archivos.py
+++ Archivos.py
@@ -7,6 +7,7 @@ class Archivos:
         lineas_archivo = []
         for linea in file.readlines():
             lineas.append(linea.replace('\n', '').split('#'))
+        file.close()
         for f in range(0, len(lineas)):
             try:
                 lineas_archivo.append([float(lineas[f][0]), float(lineas[f][1])])
@@ -23,8 +24,14 @@ class Archivos:
             lineas_resultados.append([area, perimetro])
         return lineas_resultados
 
-    def guardarResultados(self, elementos, resultados):
-
+    def guardarResultados(self, archivo, elementos, resultados):
+        file = open(archivo, 'w')
+        file.write('base,altura,area,perimetro\n')
+        for a in range(0, len(elementos)):
+            linea = str(elementos[a][0]) + ',' + str(elementos[a][1]) + ',' + str(resultados[a][0]) + ',' + str(resultados[a][1]) + '\n'
+            file.write(linea)
+        file.close()
+        return 0
 
 
 
@@ -33,7 +40,7 @@ def main():
     archivo = Archivos()
     lineas = archivo.leerArchivo('rectangulos.txt')
     resultados = archivo.calcularResultados(lineas)
-    print(resultados)
+    archivo.guardarResultados('rectangulos.csv', lineas, resultados)
 
 if __name__==  '__main__':
    main()
